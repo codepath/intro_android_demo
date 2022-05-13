0e55ff21e0e6a5ace79f2c57f411da5f6d63496f
sujune <sujune@oppo.com>
2022-05-13 14:13:26 +0800

    info:*)new

--- app/src/main/java/codepath/apps/demointroandroid/BasicTextViewActivity.java
+++ app/src/main/java/codepath/apps/demointroandroid/BasicTextViewActivity.java
@@ -11,7 +11,8 @@ public class BasicTextViewActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_basic_text_view);
-		Log.d("DEBUG", "onCreate was just called!sujune");
+		Log.d("DEBUG", "yestoday");
+		Log.d("DEBUG", "onCreate was just called!sujune test");
 		Log.d("DEBUG", "today");
 	}
 	
